4cfcaa747b193593e4ae73f144701faec72639ae
spyros <morfeas3000@gmail.com>
2017-05-15 12:27:26 +0100

    added some git config to clone oh-my-zsh cleanly
    Signed-off-by: spyros <morfeas3000@gmail.com>

--- .install.sh
+++ .install.sh
@@ -3,7 +3,15 @@
 # This is very basic cause I don't replicate the confs that often and it serves mostly as a personal note.
 # The dotfiles management was inspired by https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 
+
+#in case of probs run this: rm -rf .dotfiles .dotfiles-backup/ .gitattributes .gitconfig .gitignore .gitignore_global  .install.sh .oh-my-zsh/ .screenrc Vundle.vim/  .config/Thunar/uca.xml .config/i3 .config/i3blocks/ .funcs .hushlogin .profile .vim .vimrc .zlogout .zprofile .zshrc README.md
+#TODO: add cleanup and intelligent git merging
+
 DOT_DIR="$HOME/.dotfiles"
+
+#necessary for .oh-my-zsh
+git config --global fetch.fsckObjects false
+
 
 git clone --bare https://github.com/northdpole/dotfiles.git $DOT_DIR
 dotfiles="git --git-dir=$DOT_DIR/ --work-tree=$HOME"
@@ -22,4 +30,7 @@ sudo apt-get install zsh-syntax-highlighting zsh htop cowsay fortune-mod vim xcl
 
 # set default shell to zsh
 chsh -s /bin/zsh
+
+
+git config --global fetch.fsckObjects true
 
